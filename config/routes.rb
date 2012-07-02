4bcd153acc5cee54b5d1a161fe76e004fc9d1889
BRuno Olímpio <bruno.olimpiocosta@gmail.com>
2012-07-02 17:00:58 -0300

    Troca das routes por named routes, usando match em lugar de get. Refatoração dos testes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  
-  get "static_pages/help"
 
-  get "static_pages/about"
+# Primeira forma como escrevemos rotas.
 
-  get "static_pages/contact"
+  # get "static_pages/home"
+  # get "static_pages/help"
+  # get "static_pages/about"
+  # get "static_pages/contact"
+
+# Foi substituída por esta:
+
+  match '/home',    to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,53 +7,53 @@ describe "Static pages" do
   describe "Home page" do
 
     it "deve conter a expressao 'Sample App'" do
-      visit static_pages_home_path
+      visit home_path
       page.should have_content('Sample App')
     end
 
     it "deve conter o titulo base" do
-      visit static_pages_home_path
+      visit home_path
       page.should have_selector('title', :text => "#{base_title}")
     end
 
     it "nao deve conter o titulo Home" do
-      visit '/static_pages/home'
+      visit home_path
       page.should_not have_selector('title', :text => "Home")
     end
   end
 
   describe "Help page"	do
   	it "deve conter a expressao 'Help'" do
-  		visit static_pages_help_path
+  		visit help_path
   		page.should have_content('Help')
   	end
 
     it "deve conter o titulo 'Ruby on Rails Tutorial Sample App | Help'" do
-      visit static_pages_help_path
+      visit help_path
       page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
   describe "About page" do
   	it "deve conter a expressao 'About'" do
-  		visit static_pages_about_path
+  		visit about_path
   		page.should have_content('About')
   	end
 
     it "deve conter o titulo 'Ruby on Rails Tutorial Sample App | About Us'" do
-      visit static_pages_about_path
+      visit about_path
       page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
 
   describe "Contact page" do
     it "deve conter o h1 Contact" do
-      visit static_pages_contact_path
+      visit contact_path
       page.should have_selector('h1', text: 'Contact')
     end
 
     it "deve conter o titulo 'Ruby on Rails Tutorial Sample App | Contact'" do
-      visit static_pages_contact_path
+      visit contact_path
       page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
