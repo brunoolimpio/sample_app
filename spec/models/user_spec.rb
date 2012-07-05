39acea169143a41163fe2378dd992e27c20d3ead
BRuno Olímpio <bruno.olimpiocosta@gmail.com>
2012-07-05 15:15:59 -0300

    Implementando o modelo Users com validações de nome e email - presença, e tamanho de nome.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,42 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before do
+		@user = User.new(name: "Example User", email: "user@example.com")
+	end
+  	
+  	subject { @user }
+
+  	it { should respond_to(:name) }
+  	it { should respond_to(:email) }
+
+  	it {should be_valid}
+
+	describe "quando o nome nao for preenchido" do
+		before {@user.name=""}
+
+		it {should_not be_valid}
+	end
+
+	describe "Se o e-mail nao for preenchido" do
+		before {@user.email=""}
+
+		it {should_not be_valid}
+	end
+
+	describe "O nome nao deve ter mais de 50 caracteres" do
+		before {@user.name= "a" * 51}
+		it {should_not be_valid}
+	end
 end
