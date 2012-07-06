cf151fe5f774090506d56a1e8ceebd4c355d5f7b
BRuno Olímpio <bruno.olimpiocosta@gmail.com>
2012-07-06 00:30:20 -0300

    Inserindo validações de nome e e-mail, especialmente para garantir que o email seja unico.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -39,4 +39,34 @@ describe User do
 		before {@user.name= "a" * 51}
 		it {should_not be_valid}
 	end
+
+# Apenas algumas formas mais comuns de emails invalidos
+
+	describe "Formato de email deve ser..." do
+		it "invalido" do
+			addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+			addresses.each do |invalid_adress|
+				@user.email = invalid_adress
+				@user.should_not be_valid
+			end
+		end	
+
+	    it "deve ser valido" do
+	      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+	      addresses.each do |valid_address|
+	        @user.email = valid_address
+	        @user.should be_valid
+	      end      
+    	end
+  	end
+
+  	describe "Email deve ser unico" do
+  		before do
+  			user_with_same_email = @user.dup
+  			user_with_same_email.save
+  		end
+
+  		it {should_not be_valid}
+  	end
 end
