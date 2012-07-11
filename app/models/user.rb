571e9eba135ce77fb1b9baf8069c314fff26ab67
BRuno Olímpio <bruno.olimpiocosta@gmail.com>
2012-07-11 08:30:57 -0300

    Atualizando o annotate da tabela users no /models/users.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 # == Schema Information
-#
+# Gerado com o comando "bundle exec annotate --position before"
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  password_digest :string(255)
 #
 
 class User < ActiveRecord::Base
